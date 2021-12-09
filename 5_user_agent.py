5a4395a8697b29756911dfd698f71e4404972c20
Development-Su1 <95405810+Development-Su1@users.noreply.github.com>
2021-12-09 15:12:01 +0900

    Delete 5_user_agent.py

--- 5_user_agent.py
+++
@@ -1,9 +0,0 @@
-import requests
-url = "http://Hensuocoding.tistory.com"
-headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36"}
-res = requests.get(url, headers=headers)
-res.raise_for_status()
-with open("Hensucoding.html", "w", encoding="utf8") as f:
-    f.write(res.text)
-
-#  "User-Agent"를 해줌으로써 크롬과 같이 동일한 결과를 낼 수 있다.
