38e8644a4ca43956855f5d8c5b689e6508306d6f
Jo Hyeonsu <95405810+Development-Su1@users.noreply.github.com>
2022-01-17 14:49:53 +0900

    Update 5_user_agent.py

--- python_Webscraping/5_user_agent.py
+++ python_Webscraping/5_user_agent.py
@@ -7,3 +7,6 @@ with open("Hensucoding.html", "w", encoding="utf8") as f:
     f.write(res.text)
 
 #  "User-Agent"를 해줌으로써 크롬과 같이 동일한 결과를 낼 수 있다.
+
+# 구글에 User Agent String을 쓰면 What is my Agent?가 나오는데 그 사이트를 클릭한다.
+# 그러면 사이트 맨 위쪽에 어떠한 값이 나오는데 그것을 복사하여 headers라는 변수에 붙여넣기를 하면된다.
